d5b976f928c681c854d14a8b12f98e464891c65e
assam4 <115891697+assam4@users.noreply.github.com>
2023-12-29 13:31:09 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
 #include <vector>
-using namespace std ;
+using namespace std;
 
 template <typename RandomAccess>
 void nth_element( RandomAccess start
                 , RandomAccess element
-                , RandomAccess end )
+                , RandomAccess end) 
 {
-    while(true)
+  while (start != end )
     {
-     if(start > --end) break ;
-
-     if( *start > *element )
-        if( *element > *end )
-           swap( *start , *end );
-        else
-           swap(*start ,*element) ;
-     else 
-        if(*element > *end)
-          swap(*end ,*element) ;  
-     ++start ;
+    if  (start > element)
+        {
+        if  (*start < *element)
+        {
+            swap(*start ,*element);
+            while(element != start)
+            {
+                if(*element > *(element+1))
+                swap(*element , *(element+1));
+                ++element;
+            }
+            
+        }
+    
     }
+    else if (start < element)
+        {
+        if  (*start > *element)
+        {
+            swap(*start ,*element);
+            element = start ;
+        }
+
+        }
+        ++start ;
+    }
+}
+
+
+    
+int main ()
+{
+    vector<int> numbers = {1,3,0 ,2 ,10 ,5 ,-1};
+    nth_element(numbers.begin(),numbers.begin()+6,numbers.end());
+    for(auto var : numbers)
+    cout << var << endl; 
+    return 0 ;
+}
+
+    
+int main ()
+{
+    vector<int> numbers = {1,3,0 ,2 ,10 ,5 ,-1};
+    nth_element(numbers.begin(),numbers.begin()+6,numbers.end());
+    for(auto var : numbers)
+    cout << var << endl; 
+    return 0 ;
 }
 
 
